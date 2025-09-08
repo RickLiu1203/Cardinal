29a05e696ccf5f1cd0f6bd5b77d5cce15c427c51
Rick Liu <rickliu1203@gmail.com>
2025-09-07 22:09:52 -0400

    first commit

---
+++ Cardinal/CardinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardinalApp.swift
+//  Cardinal
+//
+//  Created by Rick Liu on 2025-09-07.
+//
+
+import SwiftUI
+
+@main
+struct CardinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
